eda71882ac9352ca7eb69260cf9a2bd6d6aba609
Jose Hilario <hilariofrotafilho@gmail.com>
2023-07-14 11:33:53 -0400

    edit

--- main.js
+++
@@ -1,7 +0,0 @@
-const toggle = document.querySelector('.nav-login.toggle')
-
-for (const element of toggle) {
-  element.addEventListener('click', function() {
-    nav.classList.toggle('show')
-  })
-}
